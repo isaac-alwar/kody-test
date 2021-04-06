bb1f41a25de12964b0629bd478e61f18555180b6
Isaac Alwar <31098369+isaac-alwar@users.noreply.github.com>
2021-04-06 13:30:47 +0100

    Working Model Complete. Tests ToDo

--- src/app/item/reducers/index.ts
+++ src/app/item/reducers/index.ts
@@ -15,34 +15,18 @@ export interface State extends fromRoot.State {
   [itemsModuleFeatureKey]: ItemState;
 }
 
-/** Provide reducer in AoT-compilation happy way */
+
 export function reducers(state: ItemState | undefined, action: Action) {
   return combineReducers({
     [fromItems.itemsFeatureKey]: fromItems.reducer,
-    // [fromItemReadingList.itemReadingListFeatureKey]: fromItemReadingList.reducer
   })(state, action);
 }
 
 
-
-
-/**
- * The createFeatureSelector function selects a piece of state from the root of the state object.
- * This is used for selecting feature states that are loaded eagerly or lazily.
- */
 export const selectItemState = createFeatureSelector<State, ItemState>(
   itemsModuleFeatureKey
 );
 
-/**
- * Every reducer module exports selector functions, however child reducers
- * have no knowledge of the overall state tree. To make them usable, we
- * need to make new selectors that wrap them.
- *
- * The createSelector function creates very efficient selectors that are memoized and
- * only recompute when arguments change. The created selectors can also be composed
- * together to select different pieces of state.
- */
 export const selectItems = createSelector(
   selectItemState,
   (state) => state[fromItems.itemsFeatureKey].items
