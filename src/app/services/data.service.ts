e846b07d6519de97d0e07cb19c3ce8861e81dbc7
Isaac Alwar <31098369+isaac-alwar@users.noreply.github.com>
2021-04-06 03:40:32 +0100

    Add NGRX Entity - models - actions

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,44 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Item } from '../model/item.model';
 
-export interface Item {
-  title: string;
-  description: string;
-}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  public items: Item[] = [
-    {
-      "title": "minim consequat",
-      "description": "Est reprehenderit excepteur nostrud laboris magna. Culpa magna cupidatat eu reprehenderit exercitation tempor exercitation sit id tempor."
-  },
-  {
-      "title": "eu proident",
-      "description": "Qui mollit dolore dolor pariatur magna fugiat anim voluptate eu. Consectetur qui aliqua irure fugiat laborum quis anim."
-  },
-  {
-      "title": "cillum in",
-      "description": "Qui cillum elit mollit nulla deserunt voluptate in et esse officia. In exercitation irure fugiat officia sint do sunt adipisicing labore reprehenderit qui in."
-  },
-  {
-      "title": "tempor sunt",
-      "description": "Nulla aliquip consectetur exercitation tempor. Reprehenderit irure enim pariatur nostrud incididunt incididunt eiusmod ullamco eu mollit et."
-  },
-  {
-      "title": "veniam cillum",
-      "description": "Sunt cupidatat duis tempor excepteur pariatur excepteur velit ad non. Et consequat officia irure cupidatat cillum ex do officia enim do et velit incididunt anim."
-  }
-  ];
+  private itemsUrl = 'https://jsonplaceholder.typicode.com/posts'; // '/assets/data/items.json';
+  public items: Item[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  public getItems(): Item[] {
-    return this.items;
+  public getAllItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.itemsUrl).pipe(map(() => this.items));
   }
 
-  public getItemById(title: string): Item {
-    return this.items[title];
-  }
+  // public getItemByTitle(title: string)  {
+  //   return this.http.get<Item>(this.itemsUrl/title);
+  // }
 }
